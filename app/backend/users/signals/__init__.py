08f376e130e813a07bfd11a621036efdf37e7db1
Sumaiya-Nawsheen <sumaiya.naw252@gmail.com>
2024-03-26 17:35:11 -0600

    update

--- app/backend/users/signals/__init__.py
+++
@@ -1,7 +0,0 @@
-# --------------------------------------------------------------
-# App imports
-# --------------------------------------------------------------
-
-from users.signals.custom_user import create_ecommerce_objects
-
-__all__ = [create_ecommerce_objects]
