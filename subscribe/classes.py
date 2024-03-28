7cacabb59bc2f734247b9eef78a0a74097fb13e4
hoque-reza <144583935+hoque-reza@users.noreply.github.com>
2024-03-27 23:43:31 -0600

    Updated the subcribe\classes.py
    Updated the subcribe\classes.py

--- subscribe/classes.py
+++ subscribe/classes.py
@@ -18,6 +18,7 @@ class Subject:
     def notify(self,subscribe,subject,message):
         pass
 
+#The update method takes subject, message, and email as arguments.
 class ConcreteObserver(Observer):
     def update(self,subject,message,email):
         print("email sent to ",email)
@@ -26,13 +27,19 @@ class ConcreteObserver(Observer):
 
 class ConcreteSubject(Subject):
     #Here subscribe is an instance of SubscribeModel and observer is an instance of Account Model
+    #The register method takes a SubscribeModel instance and an observer (user) as arguments.
     def register(self,subscribe, observer):
         subscribe.subscribers.add(observer)
 
+    #The unregister method takes a SubscribeModel instance and an observer (user) as arguments.
     def unregister(self,subscribe, observer):
         subscribe.subscribers.remove(observer)
 
     #Get all subscribers from subscribe(SubscribeModel Instance) and update observer to just send message
+        #The notify method takes a SubscribeModel instance, subject object, and message as arguments.
+        #It loops through all subscribers of the SubscribeModel instance.
+        #For each subscriber, it creates a ConcreteObserver instance and calls its update method with subject, message, and subscriber's email.
+
     def notify(self,subscribe,subject,message):
         for subscriber in subscribe.subscribers.all():
             observer=ConcreteObserver()
